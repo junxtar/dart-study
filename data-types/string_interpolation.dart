a9527699cec77afa878ec0b6dc2eeef3a4dfb821
junxtar <aa1870088@naver.com>
2023-04-14 15:30:27 +0900

    dart 문자열 연결

---
+++ data-types/string_interpolation.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  String name = 'jun';
+  int age = 25;
+  String introduce =
+      "안녕 나의 이름은 $name 이고 나의 나이는 만으로는 $age 이고 실제 나이는 ${age + 2} 이야 ";
+  print(introduce);
+
+  /*
+  변수 사용하는 방법
+
+  $달러 기호를 붙이고 사용할 변수를 적어주면 된다.
+  만약 무언가를 계산하고 싶다면 ${ } 형태로 적어주면 된다.
+   */
+}

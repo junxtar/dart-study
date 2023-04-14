6d069f8456dcb2dbe2fa82db8afb82cd133e68bf
junxtar <aa1870088@naver.com>
2023-04-14 13:30:19 +0900

    dart final변수 선언 방법

---
+++ variables/final_variables.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  final name = "jun";
+  // name = "star"; fianl로 선언한 변수는 재할당이 불가능
+  // 자바스크립트에 const 역할을 한다.
+  // 데이터 타입을 선언 여부는 사용자 마음대로
+  // 하지만 나는 구체적인 데이터 타입을 선언을 하는 것이 코드 가독성에 도움이 된다고 생각을 한다.
+
+  final int age = 2;
+  print(age);
+}
